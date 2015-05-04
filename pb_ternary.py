2070cd5ed075f32e964db37f3e877609dcb306d2
categorialNovum <beaumontjs@gmail.com>
2015-05-04 16:31:04 -0400

    conversion and addition work
    some edge cases need to be handled still

--- pb_ternary.py
+++ pb_ternary.py
@@ -31,7 +31,7 @@ def decimal_to_balanced_ternary(n):
             n = n + 1
         n = n // 3
 
-    out.reverse()
+    #out.reverse()
     return out
 
 #addition
@@ -44,39 +44,72 @@ def decimal_to_balanced_ternary(n):
 # + | 0 | + | +-|
 #----------------
 
+def add_digits(a,b):
+    '''add two ternary digits, return the result and any carry'''
+    result,carry = 0,0
+    if a is not None and b is not None:
+        if a == PLUS and b == PLUS:
+            result,carry = SUB,PLUS
+        elif a == SUB and b == SUB:
+            result,carry = PLUS,SUB
+        elif a or b == PLUS and a or b == ZERO:
+            result = PLUS
+        elif a or b == SUB and a or b == ZERO:
+            result = SUB
+        elif a or b == SUB and a or b == PLUS:
+            result = ZERO
+    elif a is None:
+        return b,carry
+    elif b is None:
+        return a,carry
+    return result,carry
+
 def add_bt(a,b):
     out = []
+    carry = 0
     a.reverse()  
     b.reverse()
+    #make sure a is the longer sequence
     if len(b) > len(a):
         tmp = b
         a = b
         b = tmp
+    #zero pad b until lengths match
+    while len(b) < len(a):
+        b.append(ZERO)
 
-    carry = False
-    for x in enumerate(a):
+    for x in range(len(a)):
         i = a[x] 
         j = b[x]
-        if i is not None and j is not None:
-            if i == PLUS and j == PLUS and not carry:
-                out.append(SUB)
-                carry = True
-            if i == PLUS and j == PLUS and carry:
-                out.append(Plus)
-                carry = True
-            if i == SUB and j == SUB and not carry:
-                out.append(PLUS)
-                carry = False
-        elif i is None:
-            out.append(j)
-        elif j is None:
-            out.append(i)
+        prev = carry
+        #print "I - ",i
+        #print "J - ",j
+        tmp,carry = add_digits(i,j)
+        #print "TMP - ",tmp
+        #print "C1 - ",carry
+        digit, junk = add_digits(tmp,prev)
+        #print "C2 - ",carry
+        #print "Digit - ",tmp
+        out.append(digit)
+    #tack on any left over carry digit
+    if carry != ZERO:
+        out.append(carry)
+    out.reverse()
+    return out
 
 def answer(n=1):
-    x = decimal_to_balanced_ternary(n) 
-    neg_x = negate_bt(x)
-    print 'ANSWER -> ',x
-    print 'NEGATIVE -> ',neg_x
+    #x = decimal_to_balanced_ternary(n) 
+    #neg_x = negate_bt(x)
+    #print 'ANSWER -> ',x
+    #print 'NEGATIVE -> ',neg_x
+    a = 2
+    b = 5
+    at = decimal_to_balanced_ternary(a)
+    bt = decimal_to_balanced_ternary(b)
+    result = add_bt(at,bt)
+    print "A (",a,") : ",at
+    print "B (",b,") : ",bt
+    print "RESULT : ",result
 
 
 if __name__ == '__main__':
